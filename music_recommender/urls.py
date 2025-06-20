f909bc4f2c3170ea017b36432ca52c6486839b78
varshreddyy <varshithareddy.per@gmail.com>
2025-06-20 15:18:19 +0530

    Integrate frontend with Django and build static files

--- music_recommender/urls.py
+++ music_recommender/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('recommender.urls')),  # ✅ Must include the recommender app
     path('', lambda request: HttpResponse("Welcome to the Music Recommender API!")),
+    path('', TemplateView.as_view(template_name='index.html')),
 ]
