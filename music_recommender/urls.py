364a446b4f0876310f4edeccb66750b12639df1a
varshreddyy <varshithareddy.per@gmail.com>
2025-06-20 17:48:12 +0530

    Serve frontend from Django

--- music_recommender/urls.py
+++ music_recommender/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+from recommender.views_frontend import index
 urlpatterns = [
+    path('', index, name='home'),
     path('admin/', admin.site.urls),
-    path('api/', include('recommender.urls')),  # ✅ Must include the recommender app
+    path('api/', include('recommender.urls')), 
     path('', lambda request: HttpResponse("Welcome to the Music Recommender API!")),
     path('', TemplateView.as_view(template_name='index.html')),
 ]
